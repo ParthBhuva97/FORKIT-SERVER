ebc58ddffc9fd672201e3bf6a6d561b82a3c2dcf
ParthBhuva97 <parthbhuva97@gmail.com>
2023-11-21 11:36:31 +0530

    Total Payable amount to the developer

--- Routes/User.js
+++ Routes/User.js
@@ -7,21 +7,28 @@ const con = require("../db");
 const router = express.Router();
 
 router.post("/updateUser", (req, res) => {
-  const data = req.body;
+  const {login,email,upi_id} = req.body;
 
-  const query =
-    "UPDATE `users` SET `email` = '" +
-    data.email +
-    "', `upi_id` = '" +
-    data.upi_id +
-    "' WHERE `users`.`user_id` = '" +
-    data.login +
-    "'";
-  console.log(query);
-  con.query(query,(err,results)=>{
-    if (err) throw res.json(err);
-    res.send(results);
-  })
+  con.query(`SELECT * FROM users WHERE user_name="${login}"`,(err,results)=>{
+    if(err) throw err;
+
+    if(results.length === 0){
+      con.query(
+        "INSERT INTO `users`(`user_name`, `email`, `upi_id`) VALUES ('"+login+"','"+email+"','"+upi_id+"')",(err,result) =>{
+          if(err) throw err;
+          res.send(result);
+        }
+      );
+    }
+    else{
+      con.query(
+        "UPDATE `users` SET `email`='"+email+"',`upi_id`='"+upi_id+"' WHERE user_name='"+login+"'",(err,result)=>{
+          if(err) throw err;
+          res.send(result);
+        }
+      );
+    }
+  });
 });
 
 router.post("/getUser", (req, res) => {
@@ -47,12 +54,13 @@ router.post("/getUser", (req, res) => {
   });
 });
 
-router.get("/getAllUsers",(req,res)=>{
-  const query = "SELECT * FROM users";
-  con.query(query, (err,results)=>{
+router.get("/getPayouts", (req, res) => {
+  const query =
+    "SELECT u.user_name, SUM(CASE WHEN p.status = 'approved' THEN p.amount ELSE 0 END) AS total_approved_amount, u.email, u.upi_id FROM users u LEFT JOIN projects p ON u.user_name = p.user_name GROUP BY u.user_name, u.email, u.upi_id";
+  con.query(query, (err, results) => {
     if (err) throw res.json(err);
     res.json(results);
   });
-})
+});
 
 module.exports = router;
